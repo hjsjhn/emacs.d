0112d87e71645b0d32a1c5395fa6c4f31c302c9a
SysCon <sys_con@outlook.com>
2019-01-18 17:15:54 +0800

    Set doom-modeline height

--- lisp/init-plugins.el
+++ lisp/init-plugins.el
@@ -33,7 +33,10 @@
 (use-package doom-modeline
   :ensure t
   :defer t
-  :hook (after-init . doom-modeline-init))
+  :hook (after-init . doom-modeline-init)
+  :config
+  (setq doom-modeline-height 24)
+  )
 
 ;; treemacs
 (use-package treemacs
