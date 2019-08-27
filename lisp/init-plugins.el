82dfc7520bf6534d902a303e9d51996596e7fc91
SysCon <sys_con@outlook.com>
2019-08-27 17:07:27 +0800

    Fix problem of create-file in counsel-find-file
    If you want to create a new file however the words of your input matches some others,you could use `C-p` and then `RET` to create it now.

--- lisp/init-plugins.el
+++ lisp/init-plugins.el
@@ -25,7 +25,9 @@
   (use-package swiper
 	:ensure t)
   (use-package counsel
-	:ensure t)
+	:ensure t
+	:config
+	(setq ivy-use-selectable-prompt t))
   (ivy-mode t)
   (setq ivy-use-virtual-buffers t)
   (setq enable-recursive-minibuffers t)
