346c5eb83d97edf3c3ccf9389c5a3caf12358d28
SysCon <sys_con@outlook.com>
2019-08-27 17:12:16 +0800

    Update init-plugins.el

--- lisp/init-plugins.el
+++ lisp/init-plugins.el
@@ -23,14 +23,13 @@
   :ensure t
   :config
   (use-package swiper
-	:ensure t)
+    :ensure t)
   (use-package counsel
-	:ensure t
-	:config
-	(setq ivy-use-selectable-prompt t))
+    :ensure t)
   (ivy-mode t)
   (setq ivy-use-virtual-buffers t)
   (setq enable-recursive-minibuffers t)
+  (setq ivy-use-selectable-prompt t)
   ;; (global-set-key (kbd "M-x") 'counsel-M-x)
   (global-set-key (kbd "C-x C-f") 'counsel-find-file)
   (global-set-key "\C-s" 'swiper))
